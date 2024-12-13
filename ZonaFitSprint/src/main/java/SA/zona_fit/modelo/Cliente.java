e57e062bcc03e5adb75036bd12b59672e3ffb872
Salvador Alcántara <141798868+salvadoralcantara@users.noreply.github.com>
2024-12-13 16:28:06 -0600

    Zona Fit App con Spring Boot
    Proyecto Zona Fit App: CRUD de clientes de un gym usando estructura básica de Spring Boot

---
+++ ZonaFitSprint/src/main/java/SA/zona_fit/modelo/Cliente.java
@@ -0,0 +1,65 @@
+package SA.zona_fit.modelo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nombre;
+    private String apellido;
+    private Integer membresia;
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", membresia=" + membresia +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Integer getMembresia() {
+        return membresia;
+    }
+
+    public void setMembresia(Integer membresia) {
+        this.membresia = membresia;
+    }
+}
+
+
---
+++ ZonaFitSprint/src/main/java/SA/zona_fit/repositorio/ClienteRepositorio.java
@@ -0,0 +1,9 @@
+package SA.zona_fit.repositorio;
+
+import SA.zona_fit.modelo.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteRepositorio extends JpaRepository<Cliente,Integer> {
+
+
+}
